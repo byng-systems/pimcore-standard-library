6ae003fae777cfc12b506dd0b7de12b0fd1b38b2
Asim Liaquat <asim@byng.co>
2016-05-05 10:14:29 +0100

    Add the default Abstract action controller

---
+++ src/Byng/Controller/AbstractAction.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Byng\Pimcore\Controller;
+
+use Pimcore\Controller\Action\Frontend;
+
+abstract class AbstractAction extends Frontend
+{
+
+    public function init()
+    {
+        parent::init();
+
+        if (\Zend_Registry::isRegistered("Zend_Locale")) {
+            $locale = \Zend_Registry::get("Zend_Locale");
+        } else {
+            $locale = new \Zend_Locale("en");
+            \Zend_Registry::set("Zend_Locale", $locale);
+        }
+
+        $this->view->language = (string) $locale;
+        $this->language = (string) $locale;
+    }
+
+}
